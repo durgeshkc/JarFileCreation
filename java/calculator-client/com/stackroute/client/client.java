45bf487d5882db4e6c95a7c555ab4b39e4170c9d
Durgesh <durgeshkumar0895@gmail.com>
2018-12-19 17:12:50 +0530

    Jar File Creation and sharing globally

---
+++ java/calculator-client/com/stackroute/client/client.java
@@ -0,0 +1,12 @@
+package com.stackroute.client;
+import com.stackroute.lib.*;
+
+public class client{
+
+	public static void main(String args[])
+	{
+		calculator c= new calculator();
+		System.out.println(c.add(2,3));
+	}
+
+}
---
+++ java/calculator-library/com/stackroute/lib/calculator.java
@@ -0,0 +1,9 @@
+package  com.stackroute.lib;
+
+public class calculator{
+public int  add(int a,int b)
+{
+return (a+b);
+}
+}
+
